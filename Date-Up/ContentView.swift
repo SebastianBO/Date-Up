4a54c8c8011134be34ed449f9fbb8620710c552c
Vader20FF <229898@edu.p.lodz.pl>
2021-08-02 15:09:29 +0200

    Initial Commit

---
+++ Date-Up/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Date-Up
+//
+//  Created by Łukasz Janiszewski on 02/08/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Date-Up/Date_UpApp.swift
@@ -0,0 +1,17 @@
+//
+//  Date_UpApp.swift
+//  Date-Up
+//
+//  Created by Łukasz Janiszewski on 02/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct Date_UpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
