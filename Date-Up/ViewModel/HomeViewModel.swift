0a90434f3e6375dd28735092c5456359976827d5
Vader20FF <229898@edu.p.lodz.pl>
2021-09-06 16:26:37 +0200

    - Started working on HomeView

---
+++ Date-Up/ViewModel/HomeViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  HomeViewModel.swift
+//  Date-Up
+//
+//  Created by Łukasz Janiszewski on 06/09/2021.
+//
+
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var allProfiles: [Profile]?
+}
